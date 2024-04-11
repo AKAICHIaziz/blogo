1c7ea8f9d0979a739db22fedf8ff91b780b21348
Med Aziz Akaichi <135503030+AKAICHIaziz@users.noreply.github.com>
2024-04-11 20:47:25 +0100

    commit changes successfully

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import "./notfound.css"
+import Image from 'next/image'
 
 
 const NotFound = () => {
@@ -10,17 +11,17 @@ const NotFound = () => {
 
         <div class="stars">
           <div class="central-body">
-            <img class="image-404" src="http://salehriaz.com/404Page/img/404.svg" width="300px" />
+            <Image class="image-404" src="http://salehriaz.com/404Page/img/404.svg" width="300px" />
               <Link href="/" class="btn-go-home" >GO BACK HOME</Link>
           </div>
           <div class="objects">
-            <img class="object_rocket" src="http://salehriaz.com/404Page/img/rocket.svg" width="40px"/>
+            <Image class="object_rocket" src="http://salehriaz.com/404Page/img/rocket.svg" width="40px"/>
               <div class="earth-moon">
-                <img class="object_earth" src="http://salehriaz.com/404Page/img/earth.svg" width="100px"/>
-                  <img class="object_moon" src="http://salehriaz.com/404Page/img/moon.svg" width="80px"/>
+                <Image class="object_earth" src="http://salehriaz.com/404Page/img/earth.svg" width="100px"/>
+                  <Image class="object_moon" src="http://salehriaz.com/404Page/img/moon.svg" width="80px"/>
                   </div>
                   <div class="box_astronaut">
-                    <img class="object_astronaut" src="http://salehriaz.com/404Page/img/astronaut.svg" width="140px"/>
+                    <Image class="object_astronaut" src="http://salehriaz.com/404Page/img/astronaut.svg" width="140px"/>
                   </div>
               </div>
               <div class="glowing_stars">
