dfff06f60fedb932376dad0c6e5399cd1d1610e7
Med Aziz Akaichi <135503030+AKAICHIaziz@users.noreply.github.com>
2024-04-11 21:23:48 +0100

    commit changes successfully

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/logo.png" ></link>
       <body className={inter.className}>
         <div className='container'>
           <Navbar />
