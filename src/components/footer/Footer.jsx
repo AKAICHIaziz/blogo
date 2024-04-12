7c6394304024c00b2e9c07267ce9685ee7d9af9b
Med Aziz Akaichi <135503030+AKAICHIaziz@users.noreply.github.com>
2024-04-12 21:50:12 +0100

    commited

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.logo}><Image src='/blogger-logo.png' width={20} height={20}/></div>
-      <div className={styles.text}>&copy; All right reserver.</div>
+      <div className={styles.text}>&copy; All right reserved PandaProd 2024-2025.</div>
     </div>
   )
 }
