8d97479f8d771aaa4fc8208df91d490ff1ffac08
Med Aziz Akaichi <135503030+AKAICHIaziz@users.noreply.github.com>
2024-04-12 21:40:08 +0100

    commited

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,10 @@
 import styles from "./footer.module.css"
+import Image from 'next/image'
 
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Logo</div>
+      <div className={styles.logo}><Image src='/blogger-logo.png' width={20} height={20}/></div>
       <div className={styles.text}>&copy; All right reserver.</div>
     </div>
   )
