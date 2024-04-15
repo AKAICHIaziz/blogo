58a608a20fa057feb77236fc511c137c26dfbb80
Med Aziz Akaichi <135503030+AKAICHIaziz@users.noreply.github.com>
2024-04-15 12:28:33 +0100

    cimmoted

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -12,6 +12,6 @@ export const connectToDb = async () => {
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
     console.log(error);
-    throw new Error(error);
+    return { error: "Something went wrong" }
   }
 };
